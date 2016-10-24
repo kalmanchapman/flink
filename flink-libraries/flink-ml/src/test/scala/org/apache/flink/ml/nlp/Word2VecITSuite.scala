14abc9f49eb894e2652cbc449e180b020e05ba42
kalman <kal.chapman@gmail.com>
2016-10-24 21:36:23 +0000

    includes test suites for Word2Vec and ContextEmbedder

---
+++ flink-libraries/flink-ml/src/test/scala/org/apache/flink/ml/nlp/Word2VecITSuite.scala
@@ -0,0 +1,8 @@
+package org.apache.flink.ml.nlp
+
+/**
+  * Created by kal on 10/15/16.
+  */
+class Word2VecITSuite {
+
+}
---
+++ flink-libraries/flink-ml/src/test/scala/org/apache/flink/ml/optimization/HuffmanBinaryTreeITSuite.scala
@@ -0,0 +1,49 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.flink.ml.optimization
+
+import org.apache.flink.api.scala._
+import org.apache.flink.ml.util.FlinkTestBase
+import org.scalatest.{FlatSpec, Matchers}
+
+class HuffmanBinaryTreeITSuite extends FlatSpec with Matchers with FlinkTestBase {
+  behavior of "ContextEmbedder for generic context learning implementation"
+
+  it should "form an initial vector set given a training corpus" in {
+    val context =
+      Seq(Context(0, Seq(0,0,0,0)),
+        Context(1, Seq(0,0,0,0)),
+        Context(2, Seq(0,0,0,0)))
+
+    val env = ExecutionEnvironment.getExecutionEnvironment
+
+    env.setParallelism(2)
+
+    val dataSet = env.fromCollection(context)
+
+    val initializedWeights = new ContextEmbedder[Int]
+      .setBatchSize(context.size)
+      .setIterations(100)
+      .createInitialWeightsDS(None, dataSet).collect().head
+
+    println("here")
+
+    initializedWeights.leafMap.size should be (3)
+  }
+}
