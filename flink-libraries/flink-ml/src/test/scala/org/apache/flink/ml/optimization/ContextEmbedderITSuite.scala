67b5af8e8224347a1ad2367f2dda702335e54915
kalman <kal.chapman@gmail.com>
2016-10-24 21:40:05 +0000

    includes tests revolving around functionality of ContextEmbedder - just checks vector creation and that learning changes initial vectors

--- flink-libraries/flink-ml/src/test/scala/org/apache/flink/ml/optimization/ContextEmbedderITSuite.scala
+++ flink-libraries/flink-ml/src/test/scala/org/apache/flink/ml/optimization/ContextEmbedderITSuite.scala
@@ -18,23 +18,47 @@
 
 package org.apache.flink.ml.optimization
 
+import org.apache.flink.api.scala._
 import org.apache.flink.ml.util.FlinkTestBase
 import org.scalatest.{FlatSpec, Matchers}
 
-class ContextEmbedderITSuite extends FlatSpec with Matchers with FlinkTestBase {
 
-  behavior of "The Word2Vec implementation"
+class ContextEmbedderITSuite extends FlatSpec with Matchers with FlinkTestBase {
+  behavior of "ContextEmbedder for generic context learning implementation"
 
   it should "form an initial vector set given a training corpus" in {
+    val context =
+      Seq(Context(0, Seq(0,0,0,0)),
+        Context(1, Seq(0,0,0,0)),
+        Context(2, Seq(0,0,0,0)))
 
+    val env = ExecutionEnvironment.getExecutionEnvironment
+
+    env.setParallelism(2)
+
+    val dataSet = env.fromCollection(context)
+
+    val initializedWeights = new ContextEmbedder[Int]
+      .setBatchSize(context.size)
+      .setTargetCount(0)
+      .setVectorSize(100)
+      .setIterations(100)
+      .createInitialWeightsDS(None, dataSet)
+
+    val initializedWeightsLocal = initializedWeights.collect().head
+
+    initializedWeightsLocal.leafMap.size should be (context.map(_.target).distinct.size)
+    initializedWeightsLocal.innerMap.size should be (initializedWeightsLocal.leafMap.size - 1)
+    initializedWeightsLocal.leafVectors.size should be (initializedWeightsLocal.leafMap.size)
+    initializedWeightsLocal.innerVectors.size should be (initializedWeightsLocal.innerMap.size)
+
+    initializedWeightsLocal.leafVectors.head._2.length should be (100)
+
+    val learnedWeights = new ContextEmbedder[Int]
+      .setBatchSize(context.size)
+      .setIterations(100)
+      .optimize(dataSet, Option(initializedWeights)).collect().head
+
+    learnedWeights.leafVectors.get(0).get should not be initializedWeightsLocal.leafVectors.get(0).get
   }
-
-  it should "learn vector representations that reflect context" in {
-
-  }
-
-  it should "learn vector representations that reflect context with initial weights provided" in {
-
-  }
-
 }
